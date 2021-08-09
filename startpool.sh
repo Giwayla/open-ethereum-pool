446f61fe38d20b7cf9ddd710e287dae0161845ef
Giwayla <dennylijun@163.com>
2021-08-09 19:39:42 +0800

    update startpool.sh

--- startpool.sh
+++ startpool.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-
-
+#Make sure no other pesky screens are running
+killall screen
+screen -wipe
+sleep 1
 #api:
 echo starting API
 screen -L -Logfile api.log -dmS api ./build/bin/open-ethereum-pool config.api.json
 
 #unlocker:
 echo starting proxy
-screen -L -Logfile proxy.log -dmS proxy ./build/bin/ethash-mining-pool config.proxy.json
+screen -L -Logfile proxy.log -dmS proxy ./build/bin/open-ethereum-pool config.proxy.json
 
 #payout:
 echo starting payout
-#screen -L -Logfile payout.log -dmS payout ./build/bin/ethash-mining-pool config.payout.json
+#screen -L -Logfile payout.log -dmS payout ./build/bin/open-ethereum-pool config.payout.json
