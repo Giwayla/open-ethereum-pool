d840baf68a38bc3cc8751eaccfa6c4821472a5d2
Giwayla <dennylijun@163.com>
2021-08-23 19:46:51 +0800

    update workers

--- proxy/miner.go
+++ proxy/miner.go
@@ -28,18 +28,24 @@ func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, param
 	share := Block{
 		number:      h.height,
 		hashNoNonce: common.HexToHash(hashNoNonce),
+		difficulty:  big.NewInt(shareDiff),
+		nonce:       nonce,
+		mixDigest:   common.HexToHash(mixDigest),
+	}
+
+	block := Block{
+		number:      h.height,
+		hashNoNonce: common.HexToHash(hashNoNonce),
 		difficulty:  h.diff,
 		nonce:       nonce,
 		mixDigest:   common.HexToHash(mixDigest),
 	}
 
-	isShare, isBlock, actualDiff, _ := hasher.VerifyShare(share, big.NewInt(shareDiff))
-
-	if !isShare {
+	if !hasher.Verify(share) {
 		return false, false
 	}
 
-	if isBlock {
+	if hasher.Verify(block) {
 		ok, err := s.rpc().SubmitBlock(params)
 		if err != nil {
 			log.Printf("Block submission failure at height %v for %v: %v", h.height, t.Header, err)
@@ -48,7 +54,7 @@ func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, param
 			return false, false
 		} else {
 			s.fetchBlockTemplate()
-			exist, err := s.backend.WriteBlock(login, id, params, shareDiff, actualDiff, h.diff.Int64(), h.height, t.Height, s.hashrateExpiration)
+			exist, err := s.backend.WriteBlock(login, id, params, shareDiff, h.diff.Int64(), h.height, s.hashrateExpiration)
 			if exist {
 				return true, false
 			}
@@ -60,8 +66,7 @@ func (s *ProxyServer) processShare(login, id, ip string, t *BlockTemplate, param
 			log.Printf("Block found by miner %v@%v at height %d", login, ip, h.height)
 		}
 	} else {
-		
-		exist, err := s.backend.WriteShare(login, id, params, shareDiff, actualDiff, h.diff.Int64(), h.height, t.Height, s.hashrateExpiration)
+		exist, err := s.backend.WriteShare(login, id, params, shareDiff, h.height, s.hashrateExpiration)
 		if exist {
 			return true, false
 		}
